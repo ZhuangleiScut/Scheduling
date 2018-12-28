350f53fe10b5668244b63b4b06e1fdb52f2289a4
[ZhuangleiScut] <[2697950380@qq.com]>
2018-12-28 10:20:03 +0800

    12-28整理前代码。完成虚拟机隔离的单次调度代码。

--- src/quit/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import random
-
-if __name__ == '__main__':
-    for i in range(1,10):
-        print(i)
-
-
--- src/quit/testrandom.py
+++
@@ -1,8 +0,0 @@
-import random
-
-list = [20, 16, 10, 5, 3, 6, 1, 9];
-random.shuffle(list)
-print("Reshuffled list : ", list)
-
-random.shuffle(list)
-print("Reshuffled list : ", list)
